dac31472c343de6e7aae891339f1d469c3951abf
Kizuno18 <110933270+Kizuno18@users.noreply.github.com>
2023-04-01 17:48:11 -0300

    Update KizuLib.lua

--- KizuLib.lua
+++ KizuLib.lua
@@ -8,14 +8,57 @@ function justT()
 end
 -- loginChar("1","1","beta.king-baiak.net",860)
 -- Realizar login no jogo
-
-function logarAcc()
-  loginChar("1","1","beta.king-baiak.net", "Account Manager",860)
-  justT()
-  g_app.setMaxFps(1)
+  
+function disableUI()
+  local rootWidget = g_ui.getRootWidget()
+  for i = 1, rootWidget:getChildCount() do
+    local childWidget = rootWidget:getChildByIndex(i)
+    childWidget:setEnabled(false)
+  end
 end
 
+function enableUI()
+  local rootWidget = g_ui.getRootWidget()
+  for i = 1, rootWidget:getChildCount() do
+    local childWidget = rootWidget:getChildByIndex(i)
+    childWidget:setEnabled(true)
+  end
+end
 
+function showError(...)
+  local errorMsgs = {...}
+  
+  -- Verifica se o widget já existe
+  local errorWindow = rootWidget:getChildById('errorWindow')
+  if not errorWindow then
+    errorWindow = g_ui.createWidget('Window', rootWidget, 'errorWindow')
+    errorWindow:setSize({ width = 310, height = 130 })
+    errorWindow:setDraggable(false)
+    
+    local errorLabel = g_ui.createWidget('Label', errorWindow)
+    errorLabel:setSize({ width = 280, height = 100 })
+    errorLabel:setPosition({ x = 10, y = 10 })
+    errorLabel:setText(table.concat(errorMsgs, "\n\n"))
+    
+    local displaySize = g_window.getUnmaximizedSize()
+    local x = (displaySize.width - errorWindow:getWidth()) / 2
+    local y = (displaySize.height - errorWindow:getHeight()) / 2
+    errorWindow:setPosition({ x = x, y = y })
+    errorWindow:show()
+  end
+end
+
+function loadBot()
+  return g_modules.getModule("game_bot"):load()
+end
+
+function loadModule(module)
+  return g_modules.getModule(module):load()
+end
+
+function unloadModule(module)
+  return g_modules.getModule(module):unload()
+end
 
 function loginChar(account, password, host, characterName, version)
   g_game.setClientVersion(version)
@@ -40,6 +83,11 @@ function partySpam()
   end, 1000)
 end
 
+function doExternalFile(filename)
+  local cmd = 'path/to/external/lua/interpreter ' .. filename
+  os.execute(cmd)
+end
+
 -- Girar o personagem em uma direção aleatória ou em todas as direções
 function dance(random, time)
   if random then
@@ -56,7 +104,11 @@ local kizuLib = {
   justT = justT,
   loginChar = loginChar,
   partySpam = partySpam,
-  dance = dance
+  dance = dance,
+  loadBot = loadBot,
+  loadModule = loadModule,
+  unloadModule = unloadModule,
+  doExternalFile = doExternalFile
 }
 
 -- Adicionar a tabela na tabela global _G, para que outras funções e scripts possam acessar as funções desta biblioteca
