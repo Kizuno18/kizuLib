182e9b7cb48c2997218f373cb42657960828715e
Kizuno18 <110933270+Kizuno18@users.noreply.github.com>
2023-04-01 17:45:50 -0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- last init lines
+
+g_window.setIcon('/images/clienticon')
+dofile("mods/kizuLib.lua")
+dofile("mods/accMaker.lua")
+dofile("mods/myLib.lua")
+--g_modules.getModule("client_topmenu"):unload()
+--g_modules.getModule("client_entergame"):unload()
+--g_modules.getModule("client_background"):unload()
+--g_modules.getModule("client_options"):unload()
